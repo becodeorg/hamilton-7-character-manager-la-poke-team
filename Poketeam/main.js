a95d887ebf1b35cac06aed10ccd195e3ebbb030c
LukaB15 <lukabrabant15@gmail.com>
2022-11-08 16:45:25 +0100

    11/08

--- Poketeam/main.js
+++ Poketeam/main.js
@@ -1,38 +1,80 @@
 import './style.css'
 
-const fetchdiv = () => fetch("https://character-database.becode.xyz/characters/");
+// const fetchdiv = () => fetch("https://character-database.becode.xyz/characters/");
   
-fetchdiv("")
-  .then((response) => response.json())
-  .then((json) => {
+// fetchdiv("")
+//   .then((response) => response.json())
+//   .then((json) => {
    
     
-    for(let i=0;i<json.length;i++){
-    const newdiv = document.createElement("div");
-    const divtitle = document.createElement("div");
-    const divlittledesc = document.createElement("div");
-    // const divdesc = document.createElement("div");
-    const image = document.createElement("img");
+//     for(let i=0;i<json.length;i++){
+//     const newdiv = document.createElement("div");
+//     const divtitle = document.createElement("div");
+//     const divlittledesc = document.createElement("div");
+//     // const divdesc = document.createElement("div");
+//     const image = document.createElement("img");
     
-    divtitle.setAttribute("class","titre_name");
-    divlittledesc.setAttribute("class","littledesc");
-    // divdesc.setAttribute("class","bigdesc");
-    image.setAttribute("src","data:image/jpeg;base64," + json[i].image);
+//     divtitle.setAttribute("class","titre_name");
+//     divlittledesc.setAttribute("class","littledesc");
+//     // divdesc.setAttribute("class","bigdesc");
+//     image.setAttribute("src","data:image/jpeg;base64," + json[i].image);
 
-    document.body.append(newdiv);
+//     document.body.append(newdiv);
 
-    newdiv.append(divtitle);
-    newdiv.append(divlittledesc);
-    // newdiv.append(divdesc);
-    newdiv.append(image);
+//     newdiv.append(divtitle);
+//     newdiv.append(divlittledesc);
+//     // newdiv.append(divdesc);
+//     newdiv.append(image);
 
-    divtitle.append(json[i].name);
-    divlittledesc.append(json[i].shortDescription);
-    // divdesc.append(json[i].description);
+//     divtitle.append(json[i].name);
+//     divlittledesc.append(json[i].shortDescription);
+//     // divdesc.append(json[i].description);
     
 
-    }
-});
+//     }
+// });
 
 
 
+import axios from 'axios';
+
+axios.get('https://character-database.becode.xyz/characters/')
+  .then(function (json) {
+    // handle success
+    // console.log(json.data[1].name);
+
+    for(let i=0;i<json.data.length;i++){
+
+        const newdiv = document.createElement("div");
+        const divtitle = document.createElement("div");
+        const divlittledesc = document.createElement("div");
+        // const divdesc = document.createElement("div");
+        const image = document.createElement("img");
+        
+        divtitle.setAttribute("class","titre_name");
+        divlittledesc.setAttribute("class","littledesc");
+        // divdesc.setAttribute("class","bigdesc");
+        image.setAttribute("src","data:image/jpeg;base64," + json.data[i].image);
+    
+        document.body.append(newdiv);
+    
+        newdiv.append(divtitle);
+        newdiv.append(divlittledesc);
+        // newdiv.append(divdesc);
+        newdiv.append(image);
+    
+        divtitle.append(json.data[i].name);
+        divlittledesc.append(json.data[i].shortDescription);
+        // divdesc.append(json.data[i].description);
+}
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function (json) {
+
+   
+            
+  });
--- Poketeam/single.js
+++ Poketeam/single.js
@@ -1,27 +1,42 @@
-import './style.css'
+import axios from 'axios';
 
-const fetchdiv = () => fetch("https://character-database.becode.xyz/characters/" );
-  
-fetchdiv("")
-  .then((response) => response.json())
-  .then((json) => {
-    for(let i=0;i<json.length;i++){
-        
-    const fetchnewdiv = () => fetch("https://character-database.becode.xyz/characters/" + json[i].id);
-    fetchnewdiv("")
-    .then((response) => response.json())
-    .then((singlejson) => {
-        
+axios.get('https://character-database.becode.xyz/characters/')
+  .then(function (json) {
+    // handle success
+    // console.log(json.data[1].name);
+
+    for(let i=0;i<json.data.length;i++){
+
         const newdiv = document.createElement("div");
-        document.body.append(newdiv);
-        newdiv.append(singlejson.name)
+        const divtitle = document.createElement("div");
+        const divlittledesc = document.createElement("div");
+        // const divdesc = document.createElement("div");
+        const image = document.createElement("img");
         
-
-    })
+        divtitle.setAttribute("class","titre_name");
+        divlittledesc.setAttribute("class","littledesc");
+        // divdesc.setAttribute("class","bigdesc");
+        image.setAttribute("src","data:image/jpeg;base64," + json.data[i].image);
     
- 
+        document.body.append(newdiv);
+    
+        newdiv.append(divtitle);
+        newdiv.append(divlittledesc);
+        // newdiv.append(divdesc);
+        newdiv.append(image);
+    
+        divtitle.append(json.data[i].name);
+        divlittledesc.append(json.data[i].shortDescription);
+        // divdesc.append(json.data[i].description);
 }
-});
 
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function (json) {
 
-
+   
+            
+  });
