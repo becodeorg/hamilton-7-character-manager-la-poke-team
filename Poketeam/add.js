4844c7991bbaf4062ef36abee924fc419cf42bc4
LukaB15 <lukabrabant15@gmail.com>
2022-11-10 11:23:16 +0100

    homepage

--- Poketeam/add.js
+++ Poketeam/add.js
@@ -1,15 +1,15 @@
-const form = document.getElementById("form");
-var base64 ;
+// const form = document.getElementById("form");
+// var base64 ;
 
-  form.addEventListener("submit", (e)=>{
-    e.preventDefault();
+  // form.addEventListener("change", (e)=>{
+  //   e.preventDefault();
 
-    const image = document.querySelector("#myFile").files[0];
+  //   const image = document.querySelector("#myFile").files[0];
 
-    console.log(document.querySelector("#name").value);
-    console.log(document.querySelector("#littledesc").value);
-    console.log(document.querySelector("#description").value);
-    console.log(document.querySelector("#myFile").files[0]);    
+  //   console.log(document.querySelector("#name").value);
+  //   console.log(document.querySelector("#littledesc").value);
+  //   console.log(document.querySelector("#description").value);
+  //   console.log(document.querySelector("#myFile").files[0]);    
     // console.log(image);
    
     // // Get the remote image as a Blob with the fetch API
@@ -23,12 +23,12 @@ var base64 ;
 
     //         const reader = new FileReader();
     //         reader.onloadend = () => {
-    //             // console.log(reader.result);
+    //             console.log(reader.result);
     //             // Logs data:image/jpeg;base64,wL2dvYWwgbW9yZ...
 
     //             // Convert to Base64 string
     //             base64 = getBase64StringFromDataURL(reader.result);
-    //             console.log(base64);
+    //             // console.log(base64);
     //             // Logs wL2dvYWwgbW9yZ...
            
     //         };
@@ -46,11 +46,11 @@ var base64 ;
 
    
 
-})
+// })
 var srcData;
 var newcharacter;
 const file = document.getElementById("myFile");
-file.addEventListener("change", test);
+file.addEventListener("submit", test);
 
 async function test(){
   const test = await encode();
@@ -61,7 +61,10 @@ async function test(){
 
  function encode() {
     var selectedfile = document.getElementById("myFile").files;
+    const getBase64StringFromDataURL = (dataURL) =>
+            dataURL.replace('data:', '').replace(/^.+,/, '');
     if (selectedfile.length > 0) {
+      
       var imageFile = selectedfile[0];
       var fileReader = new FileReader();
       fileReader.onload = function(fileLoadedEvent) {
@@ -70,7 +73,8 @@ async function test(){
         newImage.src = srcData;
         document.getElementById("dummy").innerHTML = newImage.outerHTML;
         document.getElementById("txt").value = document.getElementById("dummy").innerHTML;
-        console.log(srcData);
+        srcData = getBase64StringFromDataURL(fileReader.result);
+        // console.log(srcData);
          newcharacter = {
     
           description : document.querySelector("#description").value,
@@ -79,6 +83,14 @@ async function test(){
           name : document.querySelector("#name").value,
           image : srcData,
       }
+      
+        fetch("https://character-database.becode.xyz/characters/", {
+          method:"POST",
+          body: JSON.stringify(newcharacter),
+          headers:{
+              "Content-type": "application/json"
+          },
+        });  
       
       
       }
@@ -95,13 +107,6 @@ async function test(){
 
 
 
-// fetch("https://character-database.becode.xyz/characters/", {
-//   method:"POST",
-//   body: JSON.stringify(newcharacter),
-//   headers:{
-//       "Content-type": "application/json"
-//   },
-// });  
 
 
 
--- Poketeam/main.js
+++ Poketeam/main.js
@@ -44,41 +44,85 @@ axios.get('https://character-database.becode.xyz/characters/')
     // console.log(json.data[1].name);
 
     for(let i=0;i<json.data.length;i++){
+       
         const lien = document.createElement("a");
         const newdiv = document.createElement("div");
-        const divtitle = document.createElement("div");
-        const divlittledesc = document.createElement("div");
-        // const divdesc = document.createElement("div");
+        const card = document.createElement("div");
+        const text = document.createElement("div");
+        const name = document.createElement("h2");
+        const paragraph = document.createElement("p");
         const image = document.createElement("img");
         
-        divtitle.setAttribute("class","titre_name");
-        divlittledesc.setAttribute("class","littledesc");
-        lien.setAttribute("href","singleCharacter.html?" + json.data[i].id);
-        // divdesc.setAttribute("class","bigdesc");
-        image.setAttribute("src","data:image/jpeg;base64," + json.data[i].image);
+        newdiv.setAttribute("class","card rounded-2xl bg-green-500");
+        newdiv.setAttribute("data-tilt","");
+        newdiv.setAttribute("data-tilt-glare","");
+        newdiv.setAttribute("data-tilt-max-glare","0.5");
+        newdiv.setAttribute("data-tilt-reverse","true");
+        newdiv.setAttribute("data-tilt-scale","1.1");
+        card.setAttribute("class","card__top");
+        text.setAttribute("class","card__description rounded-2xl text-black bg-white text-center");
+        name.setAttribute("class","card__title text-2xl font-bold text-black text-center text-green-500")
 
-        const container = document.querySelector(".container");
+        lien.setAttribute("href","singleCharacter.html?" + json.data[i].id);
+        image.setAttribute("src","data:image/jpeg;base64," + json.data[i].image);
+        image.setAttribute("class","pokemon-img");
+
+        const main = document.querySelector(".main");
        
-        container.append(lien);
+        main.append(lien);
         lien.append(newdiv);
     
-        newdiv.append(divtitle);
-        newdiv.append(divlittledesc);
+        newdiv.append(card);
+        newdiv.append(text);
         // newdiv.append(divdesc);
-        newdiv.append(image);
+        card.append(image);
+        text.append(name);
+        text.append(paragraph);
     
-        divtitle.append(json.data[i].name);
-        divlittledesc.append(json.data[i].shortDescription);
-        // divdesc.append(json.data[i].description);
+        name.append(json.data[i].name);
+        paragraph.append(json.data[i].shortDescription);
 
-        // console.log(json.data[i].id);
+     
 
-        if(json.data[i].shortDescription === "Eau"){
-          newdiv.style.backgroundColor ="blue";
-      }
-      else{
-        newdiv.style.backgroundColor = "lightgrey";
-      }
+
+
+        // const lien = document.createElement("a");
+        // const newdiv = document.querySelector("newdiv");
+        // const card = document.getElementsByClassName("card");
+        // const image = document.getElementsByClassName("image");
+        // const text = document.getElementsByClassName("text");
+        // const name = document.getElementsByClassName("name");
+        // const paragraph = document.getElementsByClassName("paragraph");
+      
+        
+        
+       
+        // lien.setAttribute("href","singleCharacter.html?" + json.data[i].id);
+        // // image.setAttribute("src","data:image/jpeg;base64," + json.data[i].image);
+        // image.src = "data:image/jpeg;base64," + json.data[i].image;
+
+        // const main = document.querySelector(".main");
+       
+        // main.append(lien);
+        // lien.append(newdiv);
+    
+        // newdiv.append(card);
+        // newdiv.append(text);
+        // // newdiv.append(divdesc);
+        // card.append(image);
+        // text.append(name);
+        // text.append(paragraph);
+    
+        // name.append(json.data[i].name);
+        // paragraph.append(json.data[i].shortDescription);
+ 
+
+      //   if(json.data[i].shortDescription === "Eau"){
+      //     newdiv.style.backgroundColor ="blue";
+      // }
+      // else{
+      //   newdiv.style.backgroundColor = "lightgrey";
+      // }
 }
 
   })
@@ -91,3 +135,9 @@ axios.get('https://character-database.becode.xyz/characters/')
    
             
   });
+
+
+
+
+
+  
