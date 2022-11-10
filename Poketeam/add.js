7d41cba4d91815341ddeea4f57aa2f626bb5c8d7
LukaB15 <lukabrabant15@gmail.com>
2022-11-10 09:39:34 +0100

    jsadd

--- Poketeam/add.js
+++ Poketeam/add.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 var base64 ;
 
- form.addEventListener("submit", async (e)=>{
+  form.addEventListener("submit", (e)=>{
     e.preventDefault();
 
     const image = document.querySelector("#myFile").files[0];
@@ -12,54 +12,96 @@ var base64 ;
     console.log(document.querySelector("#myFile").files[0]);    
     // console.log(image);
    
-    // Get the remote image as a Blob with the fetch API
-    await fetch(image.src)
-        .then((res) => res.blob())
-        .then((blob) => {
-            // Read the Blob as DataURL using the FileReader API
+    // // Get the remote image as a Blob with the fetch API
+    //  fetch(image.src)
+    //     .then((res) => res.blob())
+    //     .then((blob) => {
+    //         // Read the Blob as DataURL using the FileReader API
 
-            const getBase64StringFromDataURL = (dataURL) =>
-            dataURL.replace('data:', '').replace(/^.+,/, '');
+    //         const getBase64StringFromDataURL = (dataURL) =>
+    //         dataURL.replace('data:', '').replace(/^.+,/, '');
 
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                // console.log(reader.result);
-                // Logs data:image/jpeg;base64,wL2dvYWwgbW9yZ...
+    //         const reader = new FileReader();
+    //         reader.onloadend = () => {
+    //             // console.log(reader.result);
+    //             // Logs data:image/jpeg;base64,wL2dvYWwgbW9yZ...
 
-                // Convert to Base64 string
-                base64 = getBase64StringFromDataURL(reader.result);
-                console.log(base64);
-                // Logs wL2dvYWwgbW9yZ...
+    //             // Convert to Base64 string
+    //             base64 = getBase64StringFromDataURL(reader.result);
+    //             console.log(base64);
+    //             // Logs wL2dvYWwgbW9yZ...
            
-            };
-            reader.readAsDataURL(blob);
-            
-        });
+    //         };
+    //         reader.readAsDataURL(blob);
+          
+    //     })
+    //     .then((blob) => {
+    //         console.log(base64);
+    //     })
     
-        console.log(base64);
+         
 
-    const newcharacter = {
-    
-        description : document.querySelector("#description").value,
-        shortDescription : document.querySelector("#littledesc").value,
-        id : "",
-        name : document.querySelector("#name").value,
-        image : base64,
-    }
-    
-    console.log(newcharacter);
+  
 
-//     fetch("https://character-database.becode.xyz/characters/", {
-//     method:"POST",
-//     body: JSON.stringify(newcharacter),
-//     headers:{
-//         "Content-type": "application/json"
-//     },
-// });  
+
    
 
 })
+var srcData;
+var newcharacter;
+const file = document.getElementById("myFile");
+file.addEventListener("change", test);
 
+async function test(){
+  const test = await encode();
+  console.log("oui" + test);
+  console.log(newcharacter);
+}
+
+
+ function encode() {
+    var selectedfile = document.getElementById("myFile").files;
+    if (selectedfile.length > 0) {
+      var imageFile = selectedfile[0];
+      var fileReader = new FileReader();
+      fileReader.onload = function(fileLoadedEvent) {
+         srcData = fileLoadedEvent.target.result;
+        var newImage = document.createElement('img');
+        newImage.src = srcData;
+        document.getElementById("dummy").innerHTML = newImage.outerHTML;
+        document.getElementById("txt").value = document.getElementById("dummy").innerHTML;
+        console.log(srcData);
+         newcharacter = {
+    
+          description : document.querySelector("#description").value,
+          shortDescription : document.querySelector("#littledesc").value,
+          id : "",
+          name : document.querySelector("#name").value,
+          image : srcData,
+      }
+      
+      
+      }
+      fileReader.readAsDataURL(imageFile);
+
+      
+    }
+  
+}
+
+
+
+
+
+
+
+// fetch("https://character-database.becode.xyz/characters/", {
+//   method:"POST",
+//   body: JSON.stringify(newcharacter),
+//   headers:{
+//       "Content-type": "application/json"
+//   },
+// });  
 
 
 
