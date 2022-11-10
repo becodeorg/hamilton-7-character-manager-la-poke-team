8944f8374e8fdcacaee325d3246df4c38c08143c
LukaB15 <lukabrabant15@gmail.com>
2022-11-10 13:39:57 +0100

    addjs

--- Poketeam/add.js
+++ Poketeam/add.js
@@ -50,7 +50,7 @@
 var srcData;
 var newcharacter;
 const file = document.getElementById("myFile");
-file.addEventListener("submit", test);
+file.addEventListener("change", test);
 
 async function test(){
   const test = await encode();
@@ -63,8 +63,7 @@ async function test(){
     var selectedfile = document.getElementById("myFile").files;
     const getBase64StringFromDataURL = (dataURL) =>
             dataURL.replace('data:', '').replace(/^.+,/, '');
-    if (selectedfile.length > 0) {
-      
+    if (selectedfile.length > 0) {   
       var imageFile = selectedfile[0];
       var fileReader = new FileReader();
       fileReader.onload = function(fileLoadedEvent) {
@@ -74,7 +73,7 @@ async function test(){
         document.getElementById("dummy").innerHTML = newImage.outerHTML;
         document.getElementById("txt").value = document.getElementById("dummy").innerHTML;
         srcData = getBase64StringFromDataURL(fileReader.result);
-        // console.log(srcData);
+        console.log(srcData);
          newcharacter = {
     
           description : document.querySelector("#description").value,
@@ -84,13 +83,7 @@ async function test(){
           image : srcData,
       }
       
-        fetch("https://character-database.becode.xyz/characters/", {
-          method:"POST",
-          body: JSON.stringify(newcharacter),
-          headers:{
-              "Content-type": "application/json"
-          },
-        });  
+       
       
       
       }
@@ -98,6 +91,13 @@ async function test(){
 
       
     }
+    // fetch("https://character-database.becode.xyz/characters/", {
+    //   method:"POST",
+    //   body: JSON.stringify(newcharacter),
+    //   headers:{
+    //       "Content-type": "application/json"
+    //   },
+    // });  
   
 }
 
