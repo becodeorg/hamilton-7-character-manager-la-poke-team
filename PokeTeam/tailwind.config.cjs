957f393b1155d2854f39e7efade49e3e47803770
Quentin Rouchet <quentin.rouchet@outlook.com>
2022-11-14 10:01:47 +0100

    test deploy 2

---
+++ PokeTeam/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./character-creation.html", 
+    "./single-character.html"
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
